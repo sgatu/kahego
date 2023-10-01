5e93a2b72df0cf10a7cc46d908ab2cc292eb1c96
Stefan Gatu <stefan.gatu@gmail.com>
2023-10-01 21:23:35 +0100

    feat: added memory and cpu limit by config + default buckets

--- go.mod
+++ go.mod
@@ -7,9 +7,3 @@ require github.com/joho/godotenv v1.5.1
 require github.com/inhies/go-bytesize v0.0.0-20220417184213-4913239db9cf
 
 require github.com/confluentinc/confluent-kafka-go v1.9.2
-
-require (
-	github.com/felixge/fgprof v0.9.3 // indirect
-	github.com/google/pprof v0.0.0-20211214055906-6f57359322fd // indirect
-	github.com/pkg/profile v1.7.0 // indirect
-)
---
+++ src/actors/bucketmanageractor.go
@@ -0,0 +1,82 @@
+package actors
+
+import (
+	"fmt"
+	"sync"
+
+	"sgatu.com/kahego/src/config"
+	"sgatu.com/kahego/src/streams"
+)
+
+type BucketManagerActor struct {
+	Actor
+	WaitableActor
+	bucketActors        map[string]Actor
+	BucketsConfig       map[string]config.BucketConfig
+	DefaultBucketConfig *config.BucketConfig
+	waitGroupBuckets    *sync.WaitGroup
+}
+
+func (dbma *BucketManagerActor) getBucket(bucket string) (Actor, error) {
+	if bucket, ok := dbma.bucketActors[bucket]; ok {
+		return bucket, nil
+	}
+	var config *config.BucketConfig = nil
+	if bucketConfig, ok := dbma.BucketsConfig[bucket]; ok {
+		config = &bucketConfig
+	} else {
+		config = dbma.DefaultBucketConfig
+	}
+	if config != nil {
+		bucketActor := &BucketActor{
+			Actor:           &BaseActor{},
+			WaitableActor:   &BaseWaitableActor{WaitGroup: dbma.waitGroupBuckets},
+			SupervisedActor: &BaseSupervisedActor{supervisor: dbma, id: bucket},
+			BucketConfig:    *config,
+			BucketId:        bucket,
+		}
+		dbma.bucketActors[bucket] = bucketActor
+		InitializeAndStart(bucketActor)
+		return bucketActor, nil
+	}
+	return nil, fmt.Errorf("no configuration for bucket %s available, neither a default one", bucket)
+}
+func (dbma *BucketManagerActor) DoWork(msg interface{}) (WorkResult, error) {
+
+	switch msg := msg.(type) {
+	case *streams.Message:
+		bucket, err := dbma.getBucket(msg.Bucket)
+		if err == nil {
+			Tell(bucket, msg)
+		} else {
+			fmt.Println(err)
+		}
+	case PoisonPill:
+		fmt.Println("BucketManagerActor received a PosionPill")
+		return Stop, nil
+	case IllChildMessage:
+		fmt.Printf("Bucket %s actor has died due to %s\n", msg.Id, msg.Error)
+		delete(dbma.bucketActors, msg.Id)
+	default:
+		fmt.Printf("Bucket Manager Actor received invalid message %T\n", msg)
+	}
+	return Continue, nil
+}
+
+// override GetWorkMethod
+func (dbma *BucketManagerActor) GetWorkMethod() DoWorkMethod {
+	return dbma.DoWork
+}
+
+func (dbma *BucketManagerActor) OnStart() error {
+	dbma.bucketActors = make(map[string]Actor)
+	dbma.waitGroupBuckets = &sync.WaitGroup{}
+	return nil
+}
+func (dbma *BucketManagerActor) OnStop() error {
+	for _, bucket := range dbma.bucketActors {
+		Tell(bucket, PoisonPill{})
+	}
+	dbma.waitGroupBuckets.Wait()
+	return nil
+}
